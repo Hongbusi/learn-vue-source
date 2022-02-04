e7b46ef2f85cb065413cf2f526a576fa542f5a20
Hongbusi <q951686662@qq.com>
2022-02-04 22:44:04 +0800

    chore: patch props

--- 01_渲染器实现/renderer.js
+++ 01_渲染器实现/renderer.js
@@ -15,7 +15,7 @@ const mount = (vnode, container) => {
       const value = vnode.props[key];
 
       if (key.startsWith('on')) {
-        el.addEvenListener(key.slice(2).toLowerCase(), value);
+        el.addEventListener(key.slice(2).toLowerCase(), value);
       } else {
         el.setAttribute(key, value);
       }
@@ -35,4 +35,45 @@ const mount = (vnode, container) => {
 
   // 将 el 挂载到 container 上
   container.appendChild(el);
+}
+
+const patch = (n1, n2) => {
+  if (n1.tag !== n2.tag) {
+    const n1ElParent = n1.el.parentElement;
+    n1ElParent.removeChild(n1.el);
+    mount(n2, n1ElParent);
+  } else {
+    const el = n2.el = n1.el;
+
+    // 处理 props
+    const oldProps = n1.props || {};
+    const newProps = n2.props || {};
+
+    // 将所有的 newprops 添加到 el
+    for (const key in newProps) {
+      const oldValue = oldProps[key];
+      const newValue = newProps[key];
+      if (oldValue !== newValue) {
+        if (key.startsWith('on')) {
+          el.addEventListener(key.slice(2).toLowerCase(), newValue);
+        } else {
+          el.setAttribute(key, newValue);
+        }
+      }
+    }
+
+    // 删除旧的 props
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        const value = oldProps[key];
+        if (key.startsWith('on')) {
+          el.removeEventListener(key.slice(2).toLowerCase(), value);
+        } else {
+          el.removeAttribute(key);
+        }
+      }
+    }
+
+    // 处理 children
+  }
 }
