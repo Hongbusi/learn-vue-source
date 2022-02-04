a402dc55d68b063467384a758e8bfd15ede68492
Hongbusi <q951686662@qq.com>
2022-02-04 13:08:04 +0800

    chore: renderer

--- 01_渲染器实现/renderer.js
+++ 01_渲染器实现/renderer.js
@@ -5,3 +5,34 @@ const h = (tag, props, children) => {
     children
   }
 }
+
+const mount = (vnode, container) => {
+  const el = vnode.el = document.createElement(vnode.tag);
+
+  // 处理 props
+  if (vnode.props) {
+    for (const key in vnode.props) {
+      const value = vnode.props[key];
+
+      if (key.startsWith('on')) {
+        el.addEvenListener(key.slice(2).toLowerCase(), value);
+      } else {
+        el.setAttribute(key, value);
+      }
+    }
+  }
+
+  // 处理 children
+  if (vnode.children) {
+    if (typeof vnode.children === 'string') {
+      el.textContent = vnode.children;
+    } else {
+      vnode.children.forEach(item => {
+        mount(item, el);
+      });
+    }
+  }
+
+  // 将 el 挂载到 container 上
+  container.appendChild(el);
+}
